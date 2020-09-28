75f9bb4b06736dde2c4b20e346576d476f214766
david-wells-1 <david@wells-it.co.uk>
2020-09-28 15:42:45 +0100

    add seed-job groovy script & create repo structure

---
+++ seed-job.groovy
@@ -0,0 +1,44 @@
+#!/usr/bin/env groovy
+
+GITURL = 'https://github.com'
+REPO = 'david-wells-1/jenkins-seed-job.git'
+PROJECT = 'TEST'
+
+folder("${PROJECT}")
+
+jobs = [
+    'app-1',
+    'web-1/dev',
+    'web-1/test',
+]
+
+
+// Create a Pipeline job for each path
+jobs.each { job ->
+    jobName = "${job.replace('/', '-')}"
+    jobPath = "${job}"
+    jobDisplayName = "${job.replace('/', '-')}"
+
+
+    pipelineJob("${PROJECT}/${jobName}") {
+        displayName(jobDisplayName)
+        description("Deploys the ${PROJECT}/${jobPath} Jenkinsfile")
+        parameters {
+            stringParam('Branch', 'master', '')
+        }
+
+        definition {
+            cpsScm {
+                scriptPath("${jobPath}/Jenkinsfile")
+                scm {
+                    git {
+                        branch('$Branch')
+                        remote {
+                          url("${GITURL}/${REPO}")
+                        }
+                    }
+                }    
+            }
+        }
+    }
+}
